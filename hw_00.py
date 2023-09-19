f0c5e8033530fbe6a7b5a454d1e7ee4e880d834a
Morfiny <sardor2bek3@mail.ru>
2023-09-19 14:38:19 +0300

    vs

---
+++ hw_00.py
@@ -0,0 +1,7 @@
+def my_email():
+    return "sardor2bek3@mail.ru"
+
+from operator import add, mul
+def thirteen_thirty_seven():
+
+    return add(add(add(add(mul(add(mul(add(mul(5, 5), add(10, 15)), mul(2, 5)), mul(5, 5)), 2), mul(10, 10)), mul(5, 10)), 130), 7)
